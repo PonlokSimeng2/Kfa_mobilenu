c82368ef8af23951f7a38e6717d353437da2f605
kfaitc <121995214+PonlokSimeng2@users.noreply.github.com>
2024-01-31 16:40:30 +0700

    phone otp

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,185 @@
-// ignore_for_file: unused_import, prefer_const_constructors
+// // ignore_for_file: unused_import, prefer_const_constructors
+// import 'dart:io';
+// import 'dart:async';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:get/get.dart';
+// import 'package:kfa_mobilenu/screen/Home/Home.dart';
+
+// import 'graph.dart';
+// import 'graph_report.dart';
+
+// @pragma('vm:entry-point')
+// Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+//   // If you're going to use other Firebase services in the background, such as Firestore,
+//   // make sure you call `initializeApp` before using other Firebase services.
+//   await Firebase.initializeApp();
+
+//   print("Handling a background message: ${message.messageId}");
+// }
+
+// void main() async {
+//   if (Platform.isIOS) {
+//     WidgetsFlutterBinding.ensureInitialized();
+//     // await Firebase.initializeApp(
+//     //     // options: DefaultFirebaseOptions.currentPlatform,
+//     //     );
+//   }
+//   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+//   SystemChrome.setPreferredOrientations(
+//       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetMaterialApp(debugShowCheckedModeBanner: false, home: HomePage1()
+//         //home:Body (email: '', first_name: '', from: '', gender: '', id: '', last_name: '', lat: null, log: null, tel: '', user: '',),
+//         //home: HomePage(child: Container(),)
+//         //home: Body(email: '', first_name: '', from: '', gender: '', id: '', last_name: '', lat: null, log: null, tel: '', user: '',),
+//         );
+//   }
+// }
+
+// class PhoneAuth extends StatefulWidget {
+//   const PhoneAuth({super.key});
+
+//   @override
+//   // ignore: no_logic_in_create_state
+//   State<StatefulWidget> createState() {
+//     // TODO: implement createState
+//     throw UnimplementedError();
+//   }
+// }
+// Import the generated file
+// Import the generated file
+import 'firebase_options.dart';
 import 'dart:io';
-import 'dart:async';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:get/get.dart';
-import 'package:kfa_mobilenu/screen/Home/Home.dart';
-
-import 'graph.dart';
-import 'graph_report.dart';
-
-@pragma('vm:entry-point')
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  // If you're going to use other Firebase services in the background, such as Firestore,
-  // make sure you call `initializeApp` before using other Firebase services.
-  await Firebase.initializeApp();
-
-  print("Handling a background message: ${message.messageId}");
-}
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   if (Platform.isIOS) {
     WidgetsFlutterBinding.ensureInitialized();
-    // await Firebase.initializeApp(
-    //     // options: DefaultFirebaseOptions.currentPlatform,
-    //     );
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
   }
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-  SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(debugShowCheckedModeBanner: false, home: HomePage1()
-        //home:Body (email: '', first_name: '', from: '', gender: '', id: '', last_name: '', lat: null, log: null, tel: '', user: '',),
-        //home: HomePage(child: Container(),)
-        //home: Body(email: '', first_name: '', from: '', gender: '', id: '', last_name: '', lat: null, log: null, tel: '', user: '',),
-        );
+    return const MaterialApp(
+      home: PhoneAuthScreen(),
+    );
+  }
+}
+
+class PhoneAuthScreen extends StatefulWidget {
+  const PhoneAuthScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _PhoneAuthScreenState createState() => _PhoneAuthScreenState();
+}
+
+class _PhoneAuthScreenState extends State<PhoneAuthScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  final TextEditingController _phoneNumberController = TextEditingController();
+  final TextEditingController _smsCodeController = TextEditingController();
+
+  String _verificationId = '';
+
+  Future<void> _verifyPhoneNumber() async {
+    try {
+      await _auth.verifyPhoneNumber(
+        phoneNumber: _phoneNumberController.text,
+        verificationCompleted: (PhoneAuthCredential credential) async {
+          await _auth.signInWithCredential(credential);
+          print("Phone number automatically verified");
+        },
+        verificationFailed: (FirebaseAuthException e) {
+          print("Verification failed: $e");
+        },
+        codeSent: (String verificationId, int? resendToken) {
+          print("Verification code sent to $_phoneNumberController");
+          setState(() {
+            _verificationId = verificationId;
+          });
+        },
+        codeAutoRetrievalTimeout: (String verificationId) {
+          print("Auto retrieval timeout, verificationId: $verificationId");
+        },
+        timeout: Duration(seconds: 60),
+      );
+    } catch (e) {
+      print("Error during phone number verification: $e");
+    }
+  }
+
+  Future<void> _signInWithPhoneNumber() async {
+    try {
+      final AuthCredential credential = PhoneAuthProvider.credential(
+        verificationId: _verificationId,
+        smsCode: _smsCodeController.text,
+      );
+
+      await _auth.signInWithCredential(credential);
+      print("User signed in");
+    } catch (e) {
+      print("Error during phone number sign in: $e");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Firebase Phone Auth Example'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _phoneNumberController,
+              keyboardType: TextInputType.phone,
+              decoration: InputDecoration(
+                labelText: 'Phone Number',
+              ),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: _verifyPhoneNumber,
+              child: Text('Verify Phone Number'),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              controller: _smsCodeController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'SMS Code',
+              ),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: _signInWithPhoneNumber,
+              child: Text('Sign In'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
